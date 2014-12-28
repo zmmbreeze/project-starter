bc44e4efc07a8b4c43f3f5ee46a3a3d3d6352510
zmmbreeze <zmmbreeze0825@gmail.com>
2014-12-28 23:26:33 +0800

    Update gulp tasks

--- gulp/tasks/markup.js
+++ gulp/tasks/markup.js
@@ -1,12 +1,24 @@
+var fs = require('fs');
+var path = require('path');
 var gulp = require('gulp');
 var mustache = require('gulp-mustache');
 var config = require('../config').markup
 
 gulp.task('markup', function() {
+    var partials = {};
+    fs.readdirSync(config.partialDir).forEach(function (filename) {
+        if (path.extname(filename) !== '.mustache') {
+            return;
+        }
+
+        var file = fs.readFileSync(config.partialDir + path.sep + filename, 'utf-8');
+        partials[filename.slice(0, -9)] = file;
+    });
+
     return gulp.src(config.src)
         .pipe(mustache(config.data, {
             extension: '.html'
-        }))
+        }, partials))
         .pipe(gulp.dest(config.dest));
 });
 
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -7,7 +7,7 @@ var gulp   = require('gulp');
 var config = require('../config');
 
 gulp.task('watch', ['setWatch', 'browserSync'], function() {
-    gulp.watch(config.less.src,   ['less']);
+    gulp.watch(config.less.watchSrc,   ['less']);
     gulp.watch(config.images.src, ['images']);
     gulp.watch(config.markup.src, ['markup']);
 });
